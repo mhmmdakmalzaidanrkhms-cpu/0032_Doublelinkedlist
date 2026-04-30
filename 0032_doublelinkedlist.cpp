536e7778d0ecce8334c4d2646e15bd1b15146824
zidan <mhmmdakmalzaidanrkkms@gmail.com>
2026-04-30 23:24:18 +0800

    class node

--- 0032_doublelinkedlist.cpp
+++ 0032_doublelinkedlist.cpp
@@ -2,3 +2,14 @@
 #include <iostream>
 using namespace std;
 
+// 4NimBelakang_DoubleLinkedList
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
