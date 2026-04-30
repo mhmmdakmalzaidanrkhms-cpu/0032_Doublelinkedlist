58a433c594780081fa54f34b0b508be2f1e88e71
zidan <mhmmdakmalzaidanrkkms@gmail.com>
2026-04-30 23:35:26 +0800

    Insert Di Awal

--- 0032_doublelinkedlist.cpp
+++ 0032_doublelinkedlist.cpp
@@ -30,4 +30,22 @@ public:
         Node *newNode = new Node();
         newNode->noMhs = nim;
 
+        // Insert di awal
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed\n";
+                return;
+            }
+
+            newNode->next = START;
+            if (START != NULL)
+                START->prev = newNode;
+
+            newNode->prev = NULL;
+            START = newNode;
+            return;
+        }
+
         
